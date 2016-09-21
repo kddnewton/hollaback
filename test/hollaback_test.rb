a171afdbb898c9ce498604813912837aacc46667
Kevin Deisz <kevin.deisz@gmail.com>
2016-09-21 17:24:47 -0400

    Add tests

--- test/hollaback_test.rb
+++ test/hollaback_test.rb
@@ -1,11 +1,52 @@
 require 'test_helper'
 
 class HollabackTest < Minitest::Test
-  def test_that_it_has_a_version_number
+  class Callbacker
+    attr_reader :name
+
+    def initialize(name)
+      @name = name
+    end
+
+    def say(&block)
+      puts yield
+    end
+
+    def say_hello
+      puts 'hello'
+    end
+
+    def say_goodbye
+      puts "goodbye-#{name}"
+    end
+  end
+
+  def test_version
     refute_nil ::Hollaback::VERSION
   end
 
-  def test_it_does_something_useful
-    assert false
+  def test_hollaback
+    stdout, = capture_io { sequence.call(Callbacker.new('Kevin')) }
+    assert_equal %w[hello before main after goodbye-Kevin], stdout.split("\n")
+
+    stdout, = capture_io { sequence.call(Callbacker.new('Meaghan')) }
+    assert_equal %w[hello before main after goodbye-Meaghan], stdout.split("\n")
+  end
+
+  private
+
+  def sequence
+    @sequence ||= begin
+      chain = Hollaback::Chain.new
+
+      chain.before :say_hello
+      chain.before { say { 'before' } }
+
+      chain.after { say { 'after' } }
+      chain.after(:say_goodbye)
+
+      chain.around(:say)
+      chain.compile { puts 'main' }
+    end
   end
 end
